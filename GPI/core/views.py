2c206105ccc2a434930880e8bb7c9a1045a4ff48
PATRIK2121 <130688974+PATRIK2121@users.noreply.github.com>
2024-10-31 17:39:17 -0300

    Agregar funcionalidad de guardar profesores en la BD

--- GPI/core/views.py
+++ GPI/core/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import authenticate, login
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from django.http import JsonResponse
-from .models import Modulo, DiaSemana, Asignatura, Sala
+from .models import Modulo, DiaSemana, Asignatura, Sala, Profesor
 import json
 
 
@@ -18,14 +18,47 @@ def login_view(request):
             messages.error(request, 'Usuario o contraseña incorrectos.')
     return render(request, 'templates/login.html')
 
+
+def docente_view(request):
+    if request.method == 'POST':
+        if 'eliminar' in request.POST:
+            id_profesor = request.POST.get('id_profesor')
+            profesor_a_eliminar = get_object_or_404(Profesor, id_profesor=id_profesor)
+            profesor_a_eliminar.delete()
+            # Mostrar un mensaje de éxito si es necesario
+
+        else:  # Maneja el caso de agregar un nuevo profesor
+            nombre = request.POST.get('primer_nombre').strip().capitalize()  # Eliminar espacios en blanco
+            segundo_nombre = request.POST.get('segundo_nombre').strip().capitalize()if request.POST.get('segundo_nombre') else None  # Eliminar espacios en blanco
+            apellido = request.POST.get('primer_apellido').strip().capitalize()  # Eliminar espacios en blanco
+            segundo_apellido = request.POST.get('segundo_apellido').strip().capitalize()if request.POST.get('segundo_apellido') else None  # Eliminar espacios en blanco
+
+            # Verificar que los nombres y apellidos no estén vacíos
+            if not nombre or not apellido:
+                return render(request, 'templates/docente.html', {'error': 'Los nombres y apellidos son obligatorios.'})
+
+            # Agregar el nuevo profesor a la base de datos
+            nuevo_profesor = Profesor(
+                nombre=nombre,
+                segundo_nombre=segundo_nombre,
+                apellido=apellido,
+                segundo_apellido=segundo_apellido
+            )
+            nuevo_profesor.save()
+
+    # Recuperar todos los profesores para pasar a la plantilla
+    profesores = Profesor.objects.all()
+    return render(request, 'templates/docente.html', {'profesores': profesores})
+
+
+
+
 def reemplazos_view(request):
     return render(request, 'templates/gestion_reemplazo.html')
 
 def recuperacion_view(request):
     return render(request, 'templates/gestion_recuperacion.html')
 
-def docente_view(request):
-    return render(request, 'templates/docente.html')
 
 def reportes_view(request):
     return render(request, 'templates/reportes.html')
