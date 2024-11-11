1d13f5209846e8253226d0cda6ec2b45f4ef1062
Felipe <eci.felipe@gmail.com>
2024-11-11 07:39:39 -0300

    IMPLEMENTADO PROFESOR, HORARIO Y POST
    Se implementó la creación de profesor, su horario, respectivos metodos POST para vincular ambas tablas

--- GPI/core/urls.py
+++ GPI/core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from .views import login_view, reemplazos_view, recuperacion_view, base_view, docente_view, reportes_view, CustomLogoutView, asignatura_view, sala_view
+from .views import login_view, reemplazos_view, recuperacion_view, base_view, docente_view, reportes_view, CustomLogoutView, asignatura_view, sala_view, crear_docente_view, crear_profesor_y_horarios
 
 
 urlpatterns = [
@@ -13,5 +13,7 @@ urlpatterns = [
     path('docente/', docente_view, name='docente'),
     path('reportes/', reportes_view, name='reportes'),
     path('asignaturas/', asignatura_view, name='asignatura_list'),
-    path('salas/', sala_view, name='sala_list')
+    path('salas/', sala_view, name='sala_list'),
+    path('crear_docente/', crear_docente_view, name='crear_docente'),
+    path('crear_profesor_y_horarios/',crear_profesor_y_horarios, name='crear_docente_horario')
 ]
--- GPI/core/views.py
+++ GPI/core/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
 from django.http import JsonResponse
-from .models import Modulo, DiaSemana, Asignatura, Sala, Profesor
+from .models import Modulo, DiaSemana, Asignatura, Sala, Profesor, Horario, Semestre
 import json
+import logging
 
 
 def login_view(request):
@@ -118,3 +120,91 @@ def sala_view(request):
             return JsonResponse({'message' : 'Número de la sala no proporcionado'})
         
     return JsonResponse({'error': 'Método no permitido'}, status=405)
+
+logger = logging.getLogger(__name__)
+def crear_profesor_y_horarios(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            profesor_data = data.get('profesor')
+            ultimo_profesor = Profesor.objects.last()
+            if ultimo_profesor:
+                nuevo_id_profesor = ultimo_profesor.id_profesor + 1
+            else:
+                nuevo_id_profesor = 1
+            profesor = Profesor.objects.create(
+                id_profesor=nuevo_id_profesor,
+                nombre=profesor_data['nombre'],
+                apellido=profesor_data['apellido'],
+                segundo_nombre=profesor_data.get('segundo_nombre', ''),
+                segundo_apellido=profesor_data.get('segundo_apellido', '')
+            )
+            logger.info(f"Profesor creado con ID: {profesor.id_profesor}")
+            for horario_data in data['horarios_asignados']:
+                try:
+                    asignatura = Asignatura.objects.get(id_asignatura=horario_data['asignaturaID'])
+                    sala = Sala.objects.get(id_sala=horario_data['salaID'])
+                    modulo = Modulo.objects.get(id_modulo=horario_data['moduloID'])
+                    dia = DiaSemana.objects.get(id_dia=horario_data['diaID'])
+                    semestre = Semestre.objects.get(id_semestre=horario_data['semestreID'])
+                except Exception as e:
+                    logger.error(f"Error al obtener los objetos relacionados: {e}")
+                    return JsonResponse({"error": f"Error al obtener los objetos relacionados: {str(e)}"}, status=500)
+                Horario.objects.create(
+                    profesor_id_profesor=profesor,
+                    asignatura_id_asignatura=asignatura,
+                    sala_id_sala=sala,
+                    dia_semana_id_dia=dia,
+                    modulo_id_modulo=modulo,
+                    semestre_id_semestre=semestre,
+                    seccion=horario_data['seccion'],
+                    jornada=horario_data['jornada']
+                )
+                logger.info(f"Horario creado para el profesor ID {profesor.id_profesor}.")
+
+            return JsonResponse({"message": "Profesor y horarios creados exitosamente!"}, status=201)
+        except Exception as e:
+            logger.error(f"Error inesperado: {e}")
+            return JsonResponse({"error": str(e)}, status=500)
+
+    
+def crear_docente_view(request):
+    modulos = Modulo.objects.all()
+    dias = DiaSemana.objects.all()
+    profesores = Profesor.objects.all()
+    ultimo_profesor = profesores.last()
+    ultimo_id = ultimo_profesor.id_profesor if ultimo_profesor else 0
+    context = {
+        'modulos': modulos,
+        'dias': dias,
+        'profesores': profesores,
+        'ultimo_id': ultimo_id
+    }
+    if request.method == 'POST':
+        if 'eliminar' in request.POST:
+            id_profesor = request.POST.get('id_profesor')
+            try:
+                profesor_a_eliminar = get_object_or_404(Profesor, id_profesor=id_profesor)
+                profesor_a_eliminar.delete()
+                messages.success(request, "Profesor eliminado exitosamente.")
+            except Profesor.DoesNotExist:
+                messages.error(request, "El profesor no existe.")
+        else:
+            nombre = request.POST.get('primer_nombre').strip().capitalize()
+            segundo_nombre = request.POST.get('segundo_nombre').strip().capitalize() 
+            apellido = request.POST.get('primer_apellido').strip().capitalize()
+            segundo_apellido = request.POST.get('segundo_apellido').strip().capitalize()
+
+            if not nombre or not apellido:
+                messages.error(request, 'Los nombres y apellidos son obligatorios.')
+                return redirect('docente')
+
+            nuevo_profesor = Profesor(
+                nombre=nombre,
+                segundo_nombre=segundo_nombre,
+                apellido=apellido,
+                segundo_apellido=segundo_apellido
+            )
+            nuevo_profesor.save()
+            messages.success(request, "Profesor agregado exitosamente.")
+    return render(request, 'templates/crear_docente.html',context)
